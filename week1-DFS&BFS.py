34eafce5b6ea8e924949e7badefeafa07d756d1a
exams-1 <161727693+exams-1@users.noreply.github.com>
2024-12-09 19:27:02 +0530

    Update week1-DFS&BFS.py

--- week1-DFS&BFS.py
+++ week1-DFS&BFS.py
@@ -2,19 +2,19 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.value = defaultdict(list)
-    def drawGraph(self,parent,child):
+    def DrawGraph(self, parent, child):
         self.value[parent].append(child)
-    def DFS(self,start):
+    def DFS(self, start):
         visited = [start]
         stack = [start]
-        print(stack,end=" ")
+        print(start, end = " ")
         while stack:
             s = stack[-1]
             if any(item for item in self.value[s] if item not in visited):
-                for item in[item for item in self.value[s] if item not in visited]:
-                    visited.append(item)
+                for item in [item for item in self.value[s] if item not in visited]:
                     stack.append(item)
-                    print(stack,end=" ")
+                    visited.append(item)
+                    print(item, end = " ")
                     break
             else:
                 stack.pop()
@@ -23,18 +23,19 @@ class Graph:
         queue = [start]
         while queue:
             x = queue.pop(0)
+            print(x, end = " ")
             for item in self.value[x]:
                 if item not in visited:
                     queue.append(item)
                     visited.append(item)
 g = Graph()
-g.drawGraph(1,4)
-g.drawGraph(1,2)
-g.drawGraph(2,3)
-g.drawGraph(2,6)
-g.drawGraph(4,5)
-g.drawGraph(4,7)
-g.drawGraph(7,96)
+g.DrawGraph(1,4)
+g.DrawGraph(1,2)
+g.DrawGraph(2,3)
+g.DrawGraph(2,6)
+g.DrawGraph(4,5)
+g.DrawGraph(4,7)
+g.DrawGraph(7,96)
 print("DFS Traversal:")
 g.DFS(1)
 print("\nBFS Traversal:")
